21b77a11387356a54d0e7987da7227292c78d12d
sdevmarc <suanieeee@yahoo.com>
2024-05-20 19:50:14 +0800

    feat: Add Routes component for client-side routing

---
+++ client/src/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+
+const Routes = createBrowserRouter([
+    { path: '/', element: <Home /> }
+])
+
+
+export default Routes
