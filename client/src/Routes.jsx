7976c9deb8bf529c1382aa3fb309bdf572c2afd2
Marc Edison <suanieeee@yahoo.com>
2024-05-21 04:05:44 +0800

    feat: Add routes for Menu and OrderOnline pages

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
+import Menu from './pages/Menu'
+import OrderOnline from './pages/OrderOnline'
 
 const Routes = createBrowserRouter([
-    { path: '/', element: <Home /> }
+    { path: '/', element: <Home /> },
+    { path: '/menu', element: <Menu /> },
+    { path: '/orderonline', element: <OrderOnline /> }
 ])
 
 
