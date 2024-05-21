5cc412b105c536300592cc1b7e358e45ef1bff2c
Marc Edison <suanieeee@yahoo.com>
2024-05-21 22:14:28 +0800

    feat: Add product controller for managing products

---
+++ server/controllers/Products.controller.js
@@ -0,0 +1,87 @@
+const Products = require('../models/Products.model')
+
+const ProductController = {
+    AddProduct: async (req, res) => {
+        try {
+            const values = req.body
+
+            const AddProduct = await Products.create(values)
+
+            if (AddProduct) {
+                res.json({ success: true, message: 'Item added successfully!' })
+            } else {
+                res.json({ success: false, message: 'Item failed to add!!' })
+            }
+
+        } catch (error) {
+            res.json({ success: false, message: `Error adding product controller: ${error}` })
+        }
+    },
+    GetAllProducts: async (req, res) => {
+        try {
+            const GetAllProducts = await Products.find()
+
+            res.json({ success: true, message: 'Products fetched successfully!', data: GetAllProducts })
+        } catch (error) {
+            res.json({ success: false, message: `Error getting all products controller: ${error}` })
+        }
+    },
+    SearchProduct: async (req, res) => {
+        try {
+            const { searchId } = req.params
+
+            const SearchProduct = await Products.find(
+                { $regex: new RegExp(searchId, 'i') }
+            )
+
+            res.json({ success: true, message: 'Product successfully fetched!', data: SearchProduct })
+        } catch (error) {
+            res.json({ success: false, message: `Error searching product controller: ${error}` })
+        }
+    },
+    UpdateProduct: async (req, res) => {
+        try {
+            const { productId, values } = req.body
+            const { productName, productDetails, productPhoto, productPrice } = values
+
+            const UpdateProduct = await Products.findByIdAndUpdate(
+                productId,
+                {
+                    "productInformation.productName": productName,
+                    "productInformation.productName": productDetails,
+                    "productInformation.productName": productPhoto,
+                    "productInformation.productName": productPrice,
+                },
+                {
+                    new: true
+                }
+            )
+
+            if (UpdateProduct) {
+                res.json({ success: true, message: 'Product update successfully!' })
+            } else {
+                res.json({ success: false, message: 'Product failed to update!' })
+            }
+
+        } catch (error) {
+            res.json({ success: false, message: `Error updating product controller: ${error}` })
+        }
+    },
+    DeleteProduct: async (req, res) => {
+        try {
+            const { productId } = req.params;
+
+            const DeleteProduct = await Products.findByIdAndDelete(productId);
+
+            if (DeleteProduct) {
+                res.json({ success: true, message: 'Product deleted successfully!' });
+            } else {
+                res.json({ success: false, message: 'Product deletion failed!' });
+            }
+        } catch (error) {
+            res.json({ success: false, message: `Error deleting product controller: ${error}` })
+        }
+    },
+}
+
+module.exports = ProductController
