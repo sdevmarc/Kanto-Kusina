ae8ec66a4153409647be724081a248b8cdc8a44b
Marc Edison <suanieeee@yahoo.com>
2024-05-21 22:15:03 +0800

    feat: Add Products model for managing product data

---
+++ server/models/Products.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    sellerId: {
+        type: String,
+        required: true
+    },
+    productInformation: {
+        productName: {
+            type: String,
+            required: true
+        },
+        productDetails: {
+            type: String,
+            required: true
+        },
+        productPhoto: {
+            type: String,
+            required: true
+        },
+        productPrice: {
+            type: String,
+            required: true
+        }
+    }
+}, { timestamps: true })
+
+const ProductModel = mongoose.model('Products', ProductSchema)
+module.exports = ProductModel
