187e795327c77e1eb778e4b1706a2a313bb18d19
sdevmarc <suanieeee@yahoo.com>
2024-05-24 19:18:45 +0800

    Removed

--- server/routes/Customers.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const Customers = require('../controllers/Customers.controller')
-const Middleware = require('../middleware/Customer.middleware')
-
-router.post('/addcustomer', Middleware.CreateCheckEmptyFields, Middleware.CreateCustomerCheckUsernameIfExists, Customers.AddCustomer)
-
-module.exports = router
