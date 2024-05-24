b0d96078ccb02ef0712274d8a2d1829729b72986
sdevmarc <suanieeee@yahoo.com>
2024-05-24 19:18:51 +0800

    Removed Customer related

--- server/routes/Login.routes.js
+++ server/routes/Login.routes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const LoginMiddleware = require('../middleware/Login.middleware')
 
-router.post('/loginu', LoginMiddleware.LoginCheckEmptyFields, LoginMiddleware.CheckUserUsernameIfMatched, LoginMiddleware.CheckUserPasswordIfMatched)
-router.post('/loginc', LoginMiddleware.LoginCheckEmptyFields, LoginMiddleware.CheckCustomerUsernameIfMatched, LoginMiddleware.CheckCustomerPasswordIfMatched)
+router.post('/login', LoginMiddleware.LoginCheckEmptyFields, LoginMiddleware.CheckUserUsernameIfMatched, LoginMiddleware.CheckUserPasswordIfMatched)
 
 module.exports = router
