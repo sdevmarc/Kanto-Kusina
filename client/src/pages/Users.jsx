9e80fc01815d51caae0cac300470e3a3e3d87fb1
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:47:27 +0800

    refactor: Add Users page component and related components for improved routing and functionality

---
+++ client/src/pages/Users.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import DashNavbar from '../components/DashNavbar'
+
+export default function Users() {
+    const navigate = useNavigate()
+    const [isSidebar, setSidebar] = useState(false)
+
+    useEffect(() => {
+        fetchToken()
+    }, [])
+
+    const fetchToken = () => {
+        const token = localStorage.getItem('userId')
+        if (!token) return navigate('/')
+    }
+
+    const toggleSidebar = (value) => {
+        if (value) {
+            setSidebar(true)
+        } else {
+            setSidebar(false)
+        }
+    }
+    return (
+        <>
+            <div className="flex flex-col">
+                <DashNavbar onSelectSidebar={(item) => toggleSidebar(item)} />
+                <div className="flex">
+                    <Sidebar isSidebar={isSidebar} />
+                    <div className={`${isSidebar ? 'w-[100%]' : 'w-[82.5%]'} h-[93vh] p-[1rem] flex flex-col gap-2`}>
+                        <div className="w-full h-[5%] flex justify-start items-center">
+                            <h1
+                                className='font-[600]'
+                            >
+                                Users
+                            </h1>
+                        </div>
+
+                        <div className="w-full h-[95%] flex justify-start items-start gap-5">
+                            <Link
+                                to='/users/manageusers'
+                                className="w-[20rem] h-[9rem] border border-solid rounded-lg flex flex-col justify-end items-end p-[1rem]">
+                                <h1
+                                    className='font-[600]'
+                                >
+                                    Manage Users
+                                </h1>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
