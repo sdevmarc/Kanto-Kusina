c76e9483e0d0a048068cc93e17b0dcf40f7bebb4
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:49:03 +0800

    chore: Configure server to serve uploaded files from '/uploads' directory

--- server/src/App.js
+++ server/src/App.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path');
 
 const connectDb = require('../config/dbhandler')
 
@@ -15,6 +16,7 @@ app.use(cors({
     credentials: true,
 }))
 
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(express.json())
 connectDb()
 
