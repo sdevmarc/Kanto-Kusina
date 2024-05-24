040bcba83242c5f8f53358340c872f86f25123fd
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:47:05 +0800

    refactor: Update Dashboard component to fetch token on mount and redirect to login if token is not found

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Sidebar from '../components/Sidebar'
 import DashNavbar from '../components/DashNavbar'
+import { useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
+    const navigate = useNavigate()
     const [isSidebar, setSidebar] = useState(false)
+
+    useEffect(() => {
+        fetchToken()
+    }, [])
+
+    const fetchToken = () => {
+        const token = localStorage.getItem('userId')
+        if (!token) return navigate('/')
+    }
 
     const toggleSidebar = (value) => {
         if (value) {
@@ -20,12 +31,20 @@ export default function Dashboard() {
                 <DashNavbar onSelectSidebar={(item) => toggleSidebar(item)} />
                 <div className="flex">
                     <Sidebar isSidebar={isSidebar} />
-                    <div className={`${isSidebar ? 'w-[100%]' : 'w-[82.5%]'} h-[93vh] p-[1rem] flex flex-col`}>
-                        <h1
-                            className='font-[600]'
-                        >
-                            DASHBOARD
-                        </h1>
+                    <div className={`${isSidebar ? 'w-[100%]' : 'w-[82.5%]'} h-[93vh] p-[1rem] flex flex-col gap-2`}>
+                        <div className="w-full h-[5%] flex justify-start items-center">
+                            <h1
+                                className='font-[600]'
+                            >
+                                Users
+                            </h1>
+                        </div>
+
+                        <div className="w-full h-[95%] flex justify-center items-center">
+                            <h1 className='font-[600]'>
+                                WELCOME TO DASHBOARD
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
