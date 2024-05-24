baea17c582ed19622f8d3f7cf80c479d0503a0f1
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:46:05 +0800

    feat: Add new pages and controllers for Users and Menu

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
+import Menu from './pages/Menu'
+import ManageUsers from './controllers/ManageUsers'
+import ManageMenu from './controllers/ManageMenu'
+import ViewMenu from './pages/ViewMenu'
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
-    {path:'/dashboard', element: <Dashboard />}
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/users', element: <Users /> },
+    { path: '/viewmenu', element: <ViewMenu /> },
+    { path: '/menu', element: <Menu /> },
+    { path: '/users/manageusers', element: <ManageUsers /> },
+    { path: '/menu/managemenu', element: <ManageMenu /> }
 ])
 
 
