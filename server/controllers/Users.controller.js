42cf212175b406875f9111d25c9f6088b3939588
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:48:15 +0800

    refactor: Update Users controller to add functionality for fetching and updating user data

--- server/controllers/Users.controller.js
+++ server/controllers/Users.controller.js
@@ -3,9 +3,9 @@ const Users = require('../models/Users.model')
 const UserController = {
     AddUser: async (req, res) => {
         try {
-            const values = req.body
+            const { username, password, personalDetails } = req.body
 
-            const AddUser = await Users.create(values)
+            const AddUser = await Users.create({ username, password, personalDetails })
 
             if (AddUser) {
                 res.json({ success: true, message: 'User added successfully!' })
@@ -14,6 +14,37 @@ const UserController = {
             }
         } catch (error) {
             res.json({ success: false, message: `Error adding user uontroller: ${error}` })
+        }
+    },
+    GetAllUsers: async (req, res) => {
+        try {
+            const data = await Users.find()
+            res.json({ success: 'true', message: 'Users fetched all successfully!', data })
+        } catch (error) {
+            res.json({ success: false, message: `Error get all user uontroller: ${error}` })
+        }
+    },
+    UpdateUser: async (req, res) => {
+        try {
+            const { id, personalDetails } = req.body
+            const { lastname, firstname, middlename } = personalDetails
+
+            const updateUser = await Users.findByIdAndUpdate(
+                id,
+                {
+                    "personalDetails.lastname": lastname,
+                    "personalDetails.firstname": firstname,
+                    "personalDetails.middlename": middlename
+                },
+                {
+                    new: true
+                }
+            )
+
+            res.json({ success: true, message: 'User updated successfully!', data: updateUser })
+
+        } catch (error) {
+            res.json({ success: false, message: `Error updating user uontroller: ${error}` })
         }
     }
 }
