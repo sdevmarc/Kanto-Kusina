52fade6524b05deb530d03758c6413760bb1c50d
sdevmarc <suanieeee@yahoo.com>
2024-05-24 19:19:44 +0800

    refactor: Update footer styling to use dark theme colors

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,27 +4,27 @@ import EmailIcon from '@mui/icons-material/Email'
 
 export default function Footer() {
     return (
-        <footer className="w-full h-[40dvh] bg-[#ffcb05] py-[2rem] px-[3rem] flex flex-col justify-center items-center gap-[1rem]">
-            <h1 className='font-[500] text-[1rem] text-[#000] tracking-[1rem]'>
+        <footer className="w-full h-[40dvh] bg-[#111111] py-[2rem] px-[3rem] flex flex-col justify-center items-center gap-[1rem]">
+            <h1 className='font-[500] text-[1rem] text-[#fff] tracking-[1rem]'>
                 CONNECT WITH US
             </h1>
             <div className="flex gap-[2rem]">
                 <a
                     href="https://www.facebook.com/kantokusina"
-                    className="text-[#000] text-[2rem]"
+                    className="text-[#fff] text-[2rem]"
                     target='_blank'
                 >
                     <FacebookIcon />
                 </a>
                 <a
                     href="mailto:jactubaran07@gmail.com"
-                    className="text-[#000] text-[2rem]"
+                    className="text-[#fff] text-[2rem]"
                 >
                     <EmailIcon />
                 </a>
 
             </div>
-            <div className="text-[#000] text-[.8rem] text-center">
+            <div className="text-[#fff] text-[.8rem] text-center">
                 <p>&copy; 2024 Kanto Kusina. All rights reserved.</p>
                 <p>Bonifacio Street, Barangay Dist IV, Bayombong, Philippines | +639069645470</p>
             </div>
