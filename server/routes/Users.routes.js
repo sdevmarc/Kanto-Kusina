57cadb25b8a270ccc2a8fd0eaa1ac53e11a2d909
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:48:55 +0800

    refactor: Add endpoints for fetching and updating user data in Users.routes.js

--- server/routes/Users.routes.js
+++ server/routes/Users.routes.js
@@ -5,5 +5,7 @@ const Users = require('../controllers/Users.controller')
 const Middleware = require('../middleware/User.middleware')
 
 router.post('/adduser', Middleware.CreateCheckEmptyFields, Middleware.CreateUserCheckUsernameIfExists, Users.AddUser)
+router.get('/getalluser', Users.GetAllUsers)
+router.post('/updateuser', Users.UpdateUser)
 
 module.exports = router
