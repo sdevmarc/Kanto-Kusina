47659ccb272f4af42f8f358396de995cdef7c55e
sdevmarc <suanieeee@yahoo.com>
2024-05-24 18:48:48 +0800

    feat: Add multer for handling file uploads in Products.routes.js

--- server/routes/Products.routes.js
+++ server/routes/Products.routes.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
 const Products = require('../controllers/Products.controller')
 const Middleware = require('../middleware/Product.middleware')
 
-router.post('/addproduct', Middleware.CreateProductCheckEmptyFields, Products.AddProduct)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadDir = path.join(__dirname, '..', 'uploads');
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/addproduct', upload.single('productPhoto'), Products.AddProduct)
 router.get('/getallproducts', Products.GetAllProducts)
 router.get('/searchproduct/:searchId', Middleware.SearchCheckEmptyFields, Products.SearchProduct)
 router.post('/updateproduct', Middleware.UpdateCheckEmptyFields, Products.UpdateProduct)
